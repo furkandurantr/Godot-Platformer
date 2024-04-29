cbdbf0b6465cdca5a3c7ff281f19311d92ac6904
Durkan <furkandurantr@gmail.com>
2024-04-29 23:40:29 +0300

    Cleanups for d1

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,11 +4,16 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 var canjump = true
+var wallcling = 0
 var walljump = true
+var walljampdir = 0
+var candash = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var wallgravity = ProjectSettings.get_setting("physics/2d/default_gravity")/1.5
+
+@onready var timer = $Timer
 
 @onready var audio_stream_player_2d = $AudioStreamPlayer2D
 @onready var audio_stream_player_2d_2 = $AudioStreamPlayer2D2
@@ -17,21 +22,42 @@ var wallgravity = ProjectSettings.get_setting("physics/2d/default_gravity")/1.5
 
 func _physics_process(delta):
 	# Add the gravity.
+	var direction = Input.get_axis("move_left", "move_right")
+	
 	if not is_on_floor():
 			velocity.y += gravity * delta
+			
 	if velocity.y >= 0 and is_on_wall_only():
 		pass
-		#velocity.y -= wallgravity * delta
+		#velocity.y -= wallgravity*2 * delta
 			
 	if is_on_wall_only() and Input.is_action_just_pressed("jump") and walljump:
 		velocity.y = JUMP_VELOCITY
 		walljump = false
+		velocity.x -= direction * SPEED
+		candash = false
+		walljampdir = direction
 		audio_stream_player_2d.play()
-		
-
-
+	
 	if is_on_floor():
 		canjump = true
+		wallcling = 0
+		if candash == false:
+			candash = true
+			audio_stream_player_2d_2.play()
+	
+	if Input.is_action_just_pressed("down") and is_on_floor():
+		position.y += 1
+	
+	if not is_on_wall() and Input.is_action_just_pressed("jump") and canjump == false and candash == true and direction != 0:
+		candash = false
+		velocity.x += direction * (SPEED*2)
+		#velocity.y = -100
+
+	if velocity.x >= (SPEED*2) or velocity.x <= -(SPEED*2):
+		if velocity.y > 0:
+			velocity.y = 0
+			pass
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and canjump:#and is_on_floor():
@@ -39,14 +65,8 @@ func _physics_process(delta):
 		audio_stream_player_2d.play()
 		velocity.y = JUMP_VELOCITY
 	
-	var direction = Input.get_axis("move_left", "move_right")
 	
-	if Input.is_action_just_pressed("jump"):
-		#velocity.x += direction * SPEED*2
-		pass
-		if velocity.y > 0:
-			#velocity.y = 0
-			pass
+
 		
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -61,7 +81,7 @@ func _physics_process(delta):
 	else:
 		animated_sprite.play("jump")
 
-	if !is_on_wall_only() and walljump == false:
+	if !is_on_wall_only() and walljump == false and walljampdir != direction:
 		walljump = true
 		audio_stream_player_2d_2.play()
 	
@@ -72,5 +92,5 @@ func _physics_process(delta):
 			velocity.x = lerp(velocity.x,  direction * SPEED, delta * 7.0)#0#move_toward(velocity.x, 0, SPEED)
 	else:
 		velocity.x = lerp(velocity.x,  direction * SPEED, delta * 4.0)
-
+	
 	move_and_slide()
